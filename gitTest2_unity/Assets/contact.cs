c1c9910fdf67f2abb8760da0947116b47e1a880b
Jabdoul Mayaku <ykyupro@yahoo.com>
2018-12-18 01:31:07 +0100

    push wwise
    setup wwise dans un premier temps. la session est hors du dossier unity, la soundbank est dedans (générée depuis unity)

---
+++ gitTest2_unity/Assets/contact.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class contact : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        AkSoundEngine.PostEvent("tatoe", gameObject);
+        Debug.Log("event envoyé");
+    }
+}
